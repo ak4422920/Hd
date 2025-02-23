2be899abd78f9c511f818f45158d0bef2cfa1fe8
ak4422920 <ak4422920@gmail.com>
2025-02-23 10:39:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '29171167'))
-API_HASH = environ.get('API_HASH', '7ea2149629e445936619f06a3c0dc716')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # ============================
