092f6eaec2494b5bbcd93da530fc5487f3776b93
ak4422920 <ak4422920@gmail.com>
2025-03-20 19:18:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,10 +82,10 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # ============================
 # Verification Settings
 # ============================
-VERIFY = bool(environ.get('VERIFY', False))  # Verification On (True) / Off (False)
+VERIFY = bool(environ.get('VERIFY', True))  # Verification On (True) / Off (False)
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 24))  # Add time in hours
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1002374888453'))  # Log channel id (make sure bot is admin)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/akhowtoopenlinks/34')  # How to open tutorial link for verification
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/akhowtoopenlinks/45')  # How to open tutorial link for verification
 
 # ============================
 # Link Shortener Configuration
