7cd8fd802189aee9e30004d17e7d0bc00746537c
ak4422920 <ak4422920@gmail.com>
2025-07-23 21:30:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,18 +82,18 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # ============================
 # Verification Settings
 # ============================
-VERIFY = bool(environ.get('VERIFY', False))  # Verification On (True) / Off (False)
+VERIFY = bool(environ.get('VERIFY', True))  # Verification On (True) / Off (False)
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 24))  # Add time in hours
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1002453651327'))  # Log channel id (make sure bot is admin)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/how_to_open_link_ak')  # How to open tutorial link for verification
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/how_to_open_link_ak/19')  # How to open tutorial link for verification
 
 # ============================
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '28a9d50fc1c6c070c68992e157fcd0fd9d3a9f2a')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_link_ak/18')  # Tutorial video link for opening shortlink website
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'link4url.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '420c9db1142ef6bd9020a2b9d27b7d1dd0975dcf')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_link_ak/19')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # ============================
